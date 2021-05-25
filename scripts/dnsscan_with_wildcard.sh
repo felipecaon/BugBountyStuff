58552ef71b89cd2ec63701c3908c7d57fddf6264
Felipe Caon <felipecaon@gmail.com>
2021-05-25 14:21:14 -0300

    script updates

--- scripts/dnsscan_with_wildcard.sh
+++ scripts/dnsscan_with_wildcard.sh
@@ -2,6 +2,3 @@
 # $2 list with resolvers
 
 puredns resolve $1 -r $2 --wildcard-tests 8 --write resolved_dns_domains --write-massdns massdns_like
-
-# Verify alive
-cat resolved_dns_domains | httpx -silent | anew valid_domains
