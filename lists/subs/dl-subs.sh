a658c08a0fc1cc1000d4605039363c3162677c6e
Felipe Caon <felipecaon@gmail.com>
2021-11-02 20:54:04 -0300

    remove lists, housekeeping

--- lists/subs/dl-subs.sh
+++
@@ -1,3 +0,0 @@
-wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/dns-Jhaddix.txt -O jhaddix.txt
-wget https://wordlists-cdn.assetnote.io/data/manual/2m-subdomains.txt -O 2msubs.txt
-wget https://wordlists-cdn.assetnote.io/data/manual/best-dns-wordlist.txt -O best-dns-wl.txt
