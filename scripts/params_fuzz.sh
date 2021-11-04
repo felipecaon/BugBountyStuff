962ca2aa17f9699a164bc17f8a5b8c209785749b
Felipe Caon <felipecaon@gmail.com>
2021-11-04 13:06:43 -0300

    more tools

--- scripts/params_fuzz.sh
+++ scripts/params_fuzz.sh
@@ -1,4 +1,4 @@
-# interlace -tL ~/domain_list -threads 30 -c "./params_fuzz.sh _target_ wordlist_file"
+# interlace -tL ~/domain_list -threads 30 -c "./params_fuzz.sh _target_"
 
 counter=0
 if [ "$counter" != 1 ]
@@ -9,4 +9,4 @@ fi
 
 base=$(echo $1 | sed 's/http:\/\///g' | sed 's/https:\/\///g')
 
-x8 -u $1 -w $2 -O url -o "params/$base"
+x8 -u $1 -w "~/wordlists/params" -O url -o "params/$base"
