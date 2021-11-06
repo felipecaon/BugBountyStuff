c7e33b23ac68b6d2f0b2397308a3a896450f0e5e
Felipe Caon <felipecaon@gmail.com>
2021-11-06 20:23:04 -0300

    delete fodler

--- scripts/bash-scripts/dnsscan_with_wildcard.sh
+++
@@ -1,4 +0,0 @@
-# $1 list with domains
-# $2 list with resolvers
-
-puredns resolve $1 -r $2 --wildcard-tests 8 --write resolved_dns_domains --write-massdns massdns_like
--- scripts/bash-scripts/ffuf_quick.sh
+++
@@ -1,3 +0,0 @@
-# Usage: ffuf_quick <list_of_domains>
-
-xargs -P5 -I@ sh -c 'ffuf -u @/FUZZ -w quick.txt -mc all -ac -o all.txt -maxtime 600 -v' < $1
