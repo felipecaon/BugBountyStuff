1858f1be79345fb5c205416de4ec8b1e5b5f6d50
root <root@sd-55319.dedibox.fr>
2022-08-09 02:59:07 +0200

    tweaks

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,20 +19,17 @@ wget https://go.dev/dl/go1.19.linux-amd64.tar.gz -O golang.tar.gz 1>/dev/null
 
 local="/usr/local"
 printf "$local"
-tar -xvf $local -xzf golang.tar.gz 1>/dev/null
+rm -rf /usr/local/go && tar -C /usr/local -xzf golang.tar.gz
 
 mkdir $HOME/bin
 mkdir $HOME/tools
-export GOROOT=$local/go
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:$HOME/bin
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 
-echo "export GOROOT=$local/go" >> $HOME/.bashrc
-echo "export GOPATH=$HOME/go" >> $HOME/.bashrc
-echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:$HOME/bin' >> $HOME/.bashrc
-echo 'source $HOME/.zsh_profile' >> $HOME/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bashrc
 
 source $HOME/.bashrc
+
+rm golang.tar.gz
 
 printf "[+] Done"
 
@@ -62,7 +59,7 @@ printf "Naabu:\n"
 go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 
 printf "Ffuf:\n"
-go install github.com/ffuf/ffuf@lates
+go install github.com/ffuf/ffuf@latest
 
 printf "Gau:\n"
 go install github.com/lc/gau/v2/cmd/gau@latest
@@ -79,6 +76,9 @@ go install github.com/hakluke/hakrawler@latest
 printf "MassDNS:\n"
 git clone https://github.com/blechschmidt/massdns $HOME/tools/massdns
 cd $HOME/tools/massdns && make && cp ./bin/massdns $HOME/bin
+rm -rf $HOME/tools
+mv $HOME/bin/massdns /usr/local/bin
+rm -rf $HOME/bin
 
 printf "PureDNS:\n"
 go install github.com/d3mondev/puredns/v2@latest
@@ -86,6 +86,6 @@ go install github.com/d3mondev/puredns/v2@latest
 printf "nmap:\n"
 sudo apt install -y nmap
 
-printf "Resolvers:\n"
-git clone git@github.com:felipecaon/resolvers.git
+# printf "Resolvers:\n"
+# git clone git@github.com:felipecaon/resolvers.git
 
